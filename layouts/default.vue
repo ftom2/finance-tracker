cfcffe89918155affee4990f81e94fb9225c7d60
Tomer Fishaimer <tomer.fishaimer@aquasec.com>
2024-10-25 17:13:02 +0300

    Init

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mx-auto max-w-4xl">
+    <main class="my-10">
+      <AppHeader />
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
+    },
+  ],
+});
+</script>
+
+<style>
+body {
+  font-family: "Inter", sans-serif;
+  @apply bg-white dark:bg-gray-900;
+}
+</style>
