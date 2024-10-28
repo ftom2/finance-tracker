4a39093bb317cb89246b999d428b33bcad279d95
Tomer Fishaimer <tomer.fishaimer@aquasec.com>
2024-10-28 14:28:10 +0200

    Created basic transaction form

--- components/new-transaction-modal.vue
+++ components/new-transaction-modal.vue
@@ -3,13 +3,62 @@
     <UCard>
       <template #header> Add Transaction </template>
 
-      <div>Hello</div>
+      <UForm :state="state" class="flex flex-col gap-4">
+        <UFormGroup label="Transaction type" name="type" :required="true">
+          <USelect
+            :options="TRANSACTION_TYPES"
+            placeholder="Select a Type"
+            v-model="state.type"
+          />
+        </UFormGroup>
+        <UFormGroup label="Amount" required name="amount">
+          <UInput
+            type="number"
+            placeholder="Amount"
+            v-model.number="state.amount"
+          />
+        </UFormGroup>
+        <UFormGroup label="Transaction date" required name="created_at">
+          <UInput
+            type="date"
+            icon="i-heroicons-calendar-days-20-solid"
+            v-model="state.created_at"
+          />
+        </UFormGroup>
+        <UFormGroup label="Description" name="description">
+          <UInput placeholder="description" v-model="state.description" />
+        </UFormGroup>
+        <UFormGroup label="Category" name="category" :required="true">
+          <USelect
+            :options="CATEGORIES"
+            placeholder="Select a Category"
+            v-model="state.category"
+          />
+        </UFormGroup>
+        <UButton
+          color="black"
+          variant="solid"
+          type="submit"
+          label="Save"
+          @click="isOpen = false"
+          class="self-end"
+        />
+      </UForm>
     </UCard>
   </UModal>
 </template>
 
 <script setup lang="ts">
+import { CATEGORIES, TRANSACTION_TYPES } from "~/constants";
+
 const isOpen = defineModel({ default: false });
+const state = ref({
+  type: undefined,
+  amount: 0,
+  created_at: undefined,
+  description: undefined,
+  category: undefined,
+});
 
 const emit = defineEmits(["close"]);
 </script>
--- constants.ts
+++ constants.ts
@@ -1,1 +1,13 @@
 export const transactionViewOptions = ["Yearly", "Monthly", "Daily"];
+
+export const CATEGORIES = [
+  "Food",
+  "Transport",
+  "Entertainment",
+  "Health",
+  "Education",
+  "Utilities",
+  "Others",
+];
+
+export const TRANSACTION_TYPES = ["Income", "Expense", "Savings", "Investment"];
